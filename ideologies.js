8448751f154f227c2a88415bbd520ac15b1007cb
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-11-12 22:12:52 +0800

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -1,9 +1,35 @@
 //Example
-example = { group: "Communism <span class='material-icons'>chevron_right</span> Nazism", name: "example", link: "www.google.com", 
+example = { group: "Communism<span class='material-icons'>chevron_right</span>Nazism", name: "example", link: "www.google.com", 
 desc: "Libertarianism, or more specifically Right-Libertarianism, is a political philosophy and movement that upholds liberty as a core principle. Libertarians seek to maximize autonomy and political freedom, emphasizing free association, freedom of choice, individualism and voluntary association. ", 
-thby: "test", thin: "100AD", branches: "example1, example2, example3", govt: "some view", econ: "some view", cult: "some view", dipl: "some view" };
+thby: "test", branches: "example1, example2, example3", govt: "some view", econ: "some view", cult: "some view", dipl: "some view" };
 
-//Labels
-example2 = { group: "Communism <span class='material-icons'>chevron_right</span> Nazism", name: "example", link: "www.google.com", 
-desc: "Libertarianism, or more specifically Right-Libertarianism, is a political philosophy and movement that upholds liberty as a core principle. Libertarians seek to maximize autonomy and political freedom, emphasizing free association, freedom of choice, individualism and voluntary association. ", 
-thby: "test", thin: "100AD", branches: "example1, example2, example3", govt: "some view", econ: "some view", cult: "some view", dipl: "some view" };
+//Marxism-Leninism
+polpotism = { group: "Marxism<span class='material-icons'>chevron_right</span>Marxism-Leninism", 
+name: "Pol Potism", link: "https://en.wikipedia.org/wiki/Pol_Pot#Political_ideology", 
+desc: "Pol Potism is a variant of orthodox Marxism–Leninism, but it believes in the ideal of an entirely self-sufficient and agrarian socialist society that would be entirely free from all foreign influences. It's an extreme variant of Marxism-Leninism that is considered extremely totalitarian.", 
+thby: "Pol Pot", branches: "-", govt: "Totalitarian", econ: "Communist", cult: "Somewhat Conservative", dipl: "Isolationist" };
+
+stalinism = { group: "Marxism<span class='material-icons'>chevron_right</span>Marxism-Leninism", 
+name: "Stalinism", link: "https://en.wikipedia.org/wiki/Stalinism", 
+desc: "Stalinism refers to the political ideology of Joseph Stalin. It included the creation of a one-party totalitarian police state, rapid industrialization, the theory of socialism in one country, collectivization of agriculture, and intensification of the class struggle under socialism.", 
+thby: "Joseph Stalin", branches: "-", govt: "Totalitarian", econ: "Communist", cult: "Neutral", dipl: "Nationalist" };
+
+nazbol = { group: "Marxism<span class='material-icons'>chevron_right</span>Marxism-Leninism", 
+name: "National Bolshevism", link: "https://en.wikipedia.org/wiki/National_Bolshevism", 
+desc: "National Bolshevism is a radical political movement that combines ultranationalism and communism. It is a branch of Marxism-Leninism that combines it with elements of Fascism.", 
+thby: "Miltiple, incl. Nikolay Vasilyevich Ustryalov", branches: "-", govt: "Totalitarian", econ: "Communist", cult: "Reactionary", dipl: "Ultranationalist" };
+
+strasserism = { group: "Fascism<span class='material-icons'>chevron_right</span>National Socialism", 
+name: "Strasserism", link: "https://en.wikipedia.org/wiki/Strasserism", 
+desc: "Strasserism is a strand of Nazism that calls for a more radical, mass-action and worker-based form of Nazism—hostile to Jews not from a racial, cultural or religious perspective, but from an anti-capitalist basis—to achieve a national rebirth. It derives its name from Gregor and Otto Strasser, two brothers initially associated with this position. ", 
+thby: "Joseph Stalin", branches: "-", govt: "Totalitarian", econ: "Socialist", cult: "Reactionary", dipl: "Ultranationalist" };
+
+maoism = { group: "Marxism<span class='material-icons'>chevron_right</span>Marxism-Leninism", 
+name: "Maoism", link: "https://en.wikipedia.org/wiki/Maoism", 
+desc: "Maoism is a variety of Marxism–Leninism that Mao Zedong developed for realising a socialist revolution in the agricultural, pre-industrial society of the Republic of China and later the People's Republic of China. The philosophical difference between Maoism and traditional Marxism–Leninism is that the peasantry are the revolutionary vanguard in pre-industrial societies rather than the proletariat.", 
+thby: "Mao Zedong", branches: "-", govt: "Totalitarian", econ: "Communist, Agrarian", cult: "Neutral", dipl: "Nationalist" };
+
+juche = { group: "Marxism<span class='material-icons'>chevron_right</span>Marxism-Leninism", 
+name: "Juche", link: "https://en.wikipedia.org/wiki/Juche", 
+desc: "Juche is the state ideology of North Korea, and it incorporates the historical materialist ideas of Marxism–Leninism, and strongly emphasized the individual, the nation state and its sovereignty. It claims that by becoming self-reliant and strong, a nation can achieve true socialism.", 
+thby: "Kim Il-Sung", branches: "-", govt: "Totalitarian", econ: "Communist", cult: "Neutral", dipl: "Nationalist" };
