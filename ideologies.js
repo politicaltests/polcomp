b45c576873a622d02d75bf320fdc1a71369c0c35
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-11-12 17:09:21 +0800

    Create ideologies.js

---
+++ ideologies.js
@@ -0,0 +1,9 @@
+//Example
+example = { group: "Communism <span class='material-icons'>chevron_right</span> Nazism", name: "example", link: "www.google.com", 
+desc: "Libertarianism, or more specifically Right-Libertarianism, is a political philosophy and movement that upholds liberty as a core principle. Libertarians seek to maximize autonomy and political freedom, emphasizing free association, freedom of choice, individualism and voluntary association. ", 
+thby: "test", thin: "100AD", branches: "example1, example2, example3", govt: "some view", econ: "some view", cult: "some view", dipl: "some view" };
+
+//Labels
+example2 = { group: "Communism <span class='material-icons'>chevron_right</span> Nazism", name: "example", link: "www.google.com", 
+desc: "Libertarianism, or more specifically Right-Libertarianism, is a political philosophy and movement that upholds liberty as a core principle. Libertarians seek to maximize autonomy and political freedom, emphasizing free association, freedom of choice, individualism and voluntary association. ", 
+thby: "test", thin: "100AD", branches: "example1, example2, example3", govt: "some view", econ: "some view", cult: "some view", dipl: "some view" };
